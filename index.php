8fc59b67f910241bd3ae87930c6d9c2dc7a8dc99
Hayato Ando <h.ando.creative.innovation@gmail.com>
2019-02-22 23:14:54 +0900

    第1章、第2章　まとめ

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+// チャプター1, 2
+// echo rand();
+
+// hello=helloとURLパラーメータをつけた場合、helloを受け取れる
+echo $_GET['hello'];
+// parse error
+// var_dump(rand();
+
+// fatal error
+// var_dump(commpu());
