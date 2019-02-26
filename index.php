7f80aaec84d9c43ea6d482371c7950b4264c7e5b
Hayato07 <37580224+Hayato07@users.noreply.github.com>
2019-02-27 01:18:34 +0900

    第4章　まとめ (#1)

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 // echo rand();
 
 // hello=helloとURLパラーメータをつけた場合、helloを受け取れる
-echo $_GET['hello'];
+// echo $_GET['hello'];
 // parse error
 // var_dump(rand();
 
@@ -12,24 +12,38 @@ echo $_GET['hello'];
 
 
 // チャプター3
-$age = 32;
-echo $age . "はおじさんか？";
+// $age = 32;
+// echo $age . "はおじさんか？";
 
- $a = 12;
- echo $a++; // 12
- echo $a; // 13
- echo ++$a; // 14
+//  $a = 12;
+//  echo $a++; // 12
+//  echo $a; // 13
+//  echo ++$a; // 14
 
- class SuperClass {
- }
- $a = new SuperClass;
- if ($a instanceof SuperClass) {  // インスタンスが継承したあるクラス、インターフェースを継承したものかもわかる。
-   echo "{$a}はSuperClassのインスタンス！！";
- }
+//  class SuperClass {
+//  }
+//  $a = new SuperClass;
+//  if ($a instanceof SuperClass) {  // インスタンスが継承したあるクラス、インターフェースを継承したものかもわかる。
+//    echo "{$a}はSuperClassのインスタンス！！";
+//  }
 
- $greet = $hello_flg ? $hello_flg : "bye";
- $greet = $hello_flg ?: "bye"; // 上と同じ結果になる
+//  $greet = $hello_flg ? $hello_flg : "bye";
+//  $greet = $hello_flg ?: "bye"; // 上と同じ結果になる
 
- $flag1 = true;
- $flag2 = false;
- echo $flag1 ? 1 : $flag2 ? 2 : 0; // 2  (($flag1 ? 1 :$flag2) ? 2 : 0)
+//  $flag1 = true;
+//  $flag2 = false;
+//  echo $flag1 ? 1 : $flag2 ? 2 : 0; // 2  (($flag1 ? 1 :$flag2) ? 2 : 0)
+
+
+ // チャプター4
+ function outer(){
+    $x = 1;
+    return function () use (&$x){
+              echo $x;
+              $x = $x + 1;
+            };
+  }
+  $f =  outer(); 
+$f();  // 1
+$f();  // 2
+$f();  // 3
