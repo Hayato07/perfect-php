a0bf707da41e2ac43d757d6d165b78c8ab7de289
Hayato07 <37580224+Hayato07@users.noreply.github.com>
2019-03-01 22:43:59 +0900

    チャプター４ まとめ (#2)

--- index.php
+++ index.php
@@ -36,14 +36,90 @@
 
 
  // チャプター4
- function outer(){
-    $x = 1;
-    return function () use (&$x){
-              echo $x;
-              $x = $x + 1;
-            };
-  }
-  $f =  outer(); 
-$f();  // 1
-$f();  // 2
-$f();  // 3
+//  function outer(){
+//     $x = 1;
+//     return function () use (&$x){
+//               echo $x;
+//               $x = $x + 1;
+//             };
+//   }
+//   $f =  outer(); 
+// $f();  // 1
+// $f();  // 2
+// $f();  // 3
+
+// チャプター5 
+// class Test {
+//     private $name;
+
+//   public function __construct($name)
+//     {
+//       $this->name = $name;
+//     }
+//     public function getName() {
+//       return $this->name;
+//     }
+//     public function setName(string $name) {
+//       $this->name = $name;
+//     }
+//   }
+//   $test = new Test('hayato');
+
+//   // echo $test->name; // error
+//   echo $test->getName();  // hayato
+//   $test->setName('haya');
+//   echo $test->getName(); // haya
+
+//   class MiniTest extends Test {
+//     private $type;
+//     public function __construct($name, $type) {
+//       parent::__construct($name);
+//       $this->type = $type;
+//     }
+
+//     public function getType (){
+//       return $this->type;
+//     }
+//   }
+
+//   $mini_test = new MiniTest('haya', 'math');
+//   echo $mini_test->getType();
+  // function div (int $a,int $b) {
+  //   if ($b === 0) {
+  //     throw new Exception ("$b is zero.");
+  //   }
+  //   return $a / $b;
+  // }
+
+  // try {
+  //   echo div(3, 4);
+  //   echo div(2,3);
+  //   echo div(2,0);
+  //   echo div(4, 2);
+  // } catch (Exception $e) {
+  //   echo $e->getMessage();
+  // };
+  // $a = 10;
+  // $b = $a;
+  // $c = &$a;
+
+  // $b += 2;  
+  // echo $a;
+  // $c += 2; 
+  // echo $a;
+
+  // function array_pass_ref (&$array) {
+  //   foreach ($array as $key => $value) {
+  //     $array[$key]  = $value * 2;
+  //   }
+  // }
+
+  // $a = 10;
+  // $b = 2;
+
+  // $array = [$a, $b];
+  // array_pass_ref($array); 
+  // echo $a . $b;
+  // var_dump($array);
+
+  // echo gc_enabled();
